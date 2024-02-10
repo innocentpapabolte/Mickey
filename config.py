64dac2816b2fbe37455d03dd425c051cf3dd7d20
Arush Thakur <121150107+innocentpapabolte@users.noreply.github.com>
2024-02-10 10:33:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ load_dotenv()
 API_ID = int(getenv("API_ID", 27353035))
 API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", 5350929381))
+OWNER_ID = int(getenv("OWNER_ID", 6489584045))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "Nexa_Verse")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "DadEyeBotz")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "Dev_Arora_0981")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "innocent_world_chat")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "about_meeBachaa")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "massomop")
 
 # Random Start Images
 IMG = [
@@ -67,9 +67,9 @@ STICKER = [
 
 
 EMOJIOS = [
-    "💣",
-    "💥",
-    "🪄",
+    "💋",
+    "🥵",
+    "👅",
     "🧨",
     "⚡",
     "🤡",
